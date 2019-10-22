c29596c0a32c105910213c123e6548cbaac857aa
Kam Parsen <kamparsen@yahoo.com>
2019-10-22 15:13:20 -0400

    Adde Firebase Support

---
+++ DaveTrain/DaveTrain/Shared/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DaveTrain.Shared
+{
+    public class Class22
+    {
+    }
+}
---
+++ DaveTrain/DaveTrain/Shared/Winner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DaveTrain.Shared
+{
+    public static class Winner
+    {
+        public static string Name { get; set; }
+    }
+}
